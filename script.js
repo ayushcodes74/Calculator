c035dfca7c451b545e9e6994075b89075fd40fd3
Ayush Raj <rajpiyush091@gmail.com>
2026-06-19 23:51:09 +0530

    some script file updated

---
+++ script.js
@@ -0,0 +1,64 @@
+display = document.getElementById("display")
+btn7 = document.getElementById("btn7")
+btn7.addEventListener(
+    "click" , function(){
+        display.innerText = "7"
+    }
+    
+)
+btn8 = document.getElementById("btn8")
+btn8.addEventListener(
+    "click", function() {
+        display.innerText = display.innerText + 8
+    }
+)
+btn9 = document.getElementById("btn9")
+btn9.addEventListener(
+    "click", function() {
+        display.innerText = display.innerText + 9
+    }
+)
+btn0 = document.getElementById("btn0")
+btn0.addEventListener(
+    "click", function() {
+        display.innerText = display.innerText + 0
+    }
+)
+btn1 = document.getElementById("btn1")
+btn1.addEventListener(
+    "click", function() {
+        display.innerText = display.innerText + 1
+    }
+)
+btn2 = document.getElementById("btn2")
+btn2.addEventListener(
+    "click", function() {
+        display.innerText = display.innerText + 2
+    }
+)
+btn3 = document.getElementById("btn3")
+btn3.addEventListener(
+    "click", function() {
+        display.innerText = display.innerText + 3
+    }
+)
+btn4 = document.getElementById("btn4")
+btn4.addEventListener(
+    "click", function() {
+        display.innerText = display.innerText + 4
+    }
+)
+btn5 = document.getElementById("btn5")
+btn5.addEventListener(
+    "click", function() {
+        display.innerText = display.innerText + 5
+    }
+)
+btn6 = document.getElementById("btn6")
+btn6.addEventListener(
+    "click", function() {
+        display.innerText = display.innerText + 6
+    }
+)
+
+
